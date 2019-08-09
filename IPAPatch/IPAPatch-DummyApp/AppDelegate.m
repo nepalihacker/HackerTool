8b13ecdc0429598b86eb8487a6b13cdcec09d86c
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-08-09 10:30:35 -0400

    Create AppDelegate.m

---
+++ IPAPatch/IPAPatch-DummyApp/AppDelegate.m
@@ -0,0 +1,12 @@
+// ⚠️ Note: This is placeholder target for installing the ipa file
+//    DO NOT MODIFY.
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+@end

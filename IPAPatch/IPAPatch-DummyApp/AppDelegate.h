ac31e5e35c0849ec3620b676d109a4051884cd02
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-08-09 10:30:57 -0400

    Create AppDelegate.h

---
+++ IPAPatch/IPAPatch-DummyApp/AppDelegate.h
@@ -0,0 +1,11 @@
+// ⚠️ Note: This is placeholder target for installing the ipa file
+//    DO NOT MODIFY.
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
